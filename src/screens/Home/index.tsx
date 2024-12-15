0bd3cbeda62c5ec1c16c57ada80e41168cb37eee
Marcos Andrade <marcospandrade2@gmail.com>
2024-12-15 09:18:04 -0400

    feat: create form to add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  function handleAddParticipant() {
+    console.log("Adicionando participante");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Nome do Evento</Text>
-      <Text style={styles.subtitle}>Sexta, 4 de Novembro de 2024</Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,15 +7,47 @@ export const styles = StyleSheet.create({
     padding: 24,
   },
 
-  title: {
+  eventName: {
     color: "#FDFCFE",
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 48,
   },
 
-  subtitle: {
+  eventDate: {
     color: "#6B6B6B",
     fontSize: 16,
   },
+
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42
+  },
+
+  input: {
+    flex: 1,
+    backgroundColor: "#1F1E25",
+    height: 56,
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12
+  },
+
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  }
 });
